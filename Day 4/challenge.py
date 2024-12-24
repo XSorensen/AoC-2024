c5aebf0b29f66b3300d1503965dabd7db78366a2
buddy <145794157+XSorensen@users.noreply.github.com>
2024-12-24 15:22:32 -0800

    Day 4 Complete

--- Day 4/challenge.py
+++ Day 4/challenge.py
@@ -1,12 +1,13 @@
-
-
-
 def main():
     print("Task 1:")
     task1()
 
+    print("\nTask 2:")
+    task2()
+
     return None
 
+# find all occurences of XMAS in the puzzle (left, right, diagonal, backwards)
 def task1():
     with open("input.txt", "r") as inFile:
         stream = inFile.read()
@@ -24,18 +25,41 @@ def task1():
     for i in range(rows):
         for j in range(cols):
             if(lines[i][j] == "X"):
-                xmas_count += count_xmas_from_index(grid, i, j, rows, cols)
+                xmas_count += count_xmas_from_index_task1(grid, i, j, rows, cols)
 
     print(f"Total: {xmas_count}")
 
     return None
 
-def count_xmas_from_index(grid, row, col, row_count, col_count):
+def task2():
+    with open("input.txt", "r") as inFile:
+        stream = inFile.read()
+
+    lines = stream.splitlines()    
+    grid = []
+
+    rows = len(lines)
+    cols = len(lines[0])
+
+    for line in lines:
+        grid.append(list(line))
+
+    xmas_count = 0
+    for i in range(rows):
+        for j in range(cols):
+            if(lines[i][j] == "A"):
+                if(is_valid_xmas_from_index_task2(grid, i, j, rows, cols)):
+                    xmas_count += 1
+
+    print(f"Total: {xmas_count}")
+
+    return None
+
+def count_xmas_from_index_task1(grid, row, col, row_count, col_count):
     match = ["M", "A", "S"]
     toCheck = []
     # xmas length = 4
     # there must be at least 3 open positions between x and the nearest barrier
-    
     
     # vertical bottom up XMAS
     if(row >= 3):
@@ -78,6 +102,33 @@ def count_xmas_from_index(grid, row, col, row_count, col_count):
             count += 1
 
     return count
+
+def is_valid_xmas_from_index_task2(grid, row, col, row_count, col_count):
+    # early exit if potential xmas does not have enough room
+    if(row < 1 or row > row_count - 2):
+        return False
+    elif(col < 1 or col > col_count - 2):
+        return False
+
+    valid_cross = True
+
+    # top left to bottom right diagonal
+    if((grid[row - 1][col - 1] == "M" and grid[row + 1][col + 1] == "S")):
+        valid_cross = True
+    elif((grid[row - 1][col - 1] == "S" and grid[row + 1][col + 1] == "M")):
+        valid_cross = True
+    else:
+        valid_cross = False
+    
+    # bottom left to bottom right diagonal
+    if((grid[row + 1][col - 1] == "M" and grid[row - 1][col + 1] == "S")):
+        valid_cross = valid_cross
+    elif((grid[row + 1][col - 1] == "S" and grid[row - 1][col + 1] == "M")):
+        valid_cross = valid_cross
+    else:
+        valid_cross = False
+    
+    return valid_cross
 
 if __name__ == "__main__":
     main()
