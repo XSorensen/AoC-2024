92acf184414bce4f08164ad5b9eedc6651dda56c
buddy <145794157+XSorensen@users.noreply.github.com>
2024-12-24 16:58:12 -0800

    Extracted reading input functionality to its own method

--- Day 5/challenge.py
+++ Day 5/challenge.py
@@ -9,6 +9,21 @@ def main():
     return None
 
 def task1():
+    rules, updates = extractData()
+
+    sum_of_middle_pages = 0
+    for update in updates:
+        if(valid_update(update, rules)):
+            sum_of_middle_pages += update[len(update) // 2]
+
+    print(f"Total: {sum_of_middle_pages}")
+
+    return None
+
+def task2():
+    return None
+
+def extractData():
     with open("rules.txt", "r") as inFile:
         stream = inFile.read()    
         pattern = "(\\d+)\\|(\\d+)"
@@ -30,16 +45,8 @@ def task1():
             rules_dict[int(rule[0])].append(int(rule[1]))
         else:
             rules_dict[int(rule[0])] = [int(rule[1])]
-
-
-    sum_of_middle_pages = 0
-    for update in int_updates:
-        if(valid_update(update, rules_dict)):
-            sum_of_middle_pages += update[len(update) // 2]
-
-    print(f"Total: {sum_of_middle_pages}")
-
-    return None
+    
+    return [rules_dict, int_updates]
 
 def valid_update(update, rules):
     printed_pages = []
@@ -55,9 +62,6 @@ def valid_update(update, rules):
         printed_pages.append(page)    
 
     return True
-
-def task2():
-    return None
 
 if __name__ == "__main__":
     main()
